2abf077c0bdebb0182386d9a419725cb6aac4d02
Mehdi Amini <mehdi.amini@silkan.com>
2014-04-23 01:06:28 -0700

    Fix modulo performance
    Thanks coyotte for hinting.
    Note that the branch is remove by both GCC and Clang.

--- pythran/pythonic/numpy/mod.hpp
+++ pythran/pythonic/numpy/mod.hpp
@@ -2,16 +2,42 @@
 #define PYTHONIC_NUMPY_MOD_HPP
 
 #include "pythonic/utils/proxy.hpp"
-#include "pythonic/types/ndarray.hpp"
+#include "pythonic/types/assignable.hpp"
 #include "pythonic/operator_/mod.hpp"
+
 
 namespace pythonic {
 
-    namespace numpy {
-#define NUMPY_BINARY_FUNC_NAME mod
-#define NUMPY_BINARY_FUNC_SYM operator_::mod
-#include "pythonic/types/numpy_binary_expr.hpp"
+    namespace operator_ {
 
+        template<class A, class B>
+            auto mod(A const& a, B const& b)
+            -> typename std::enable_if<not std::is_fundamental<B>::value,
+                                       typename assignable<decltype(a%b)>::type>::type
+            {
+                auto t = a % b;
+                auto mask = t<0;
+                t[mask] += b[mask];
+                return t;
+            }
+
+        template<class A, class B>
+            auto mod(A const& a, B const& b)
+            -> typename std::enable_if<std::is_fundamental<B>::value and not std::is_fundamental<A>::value,
+                                       typename assignable<decltype(a%b)>::type>::type
+            {
+                auto t = a % b;
+                auto mask = t<0;
+                t[mask] += b;
+                return t;
+            }
+
+    }
+
+    namespace numpy {
+        using operator_::mod;
+
+        PROXY(pythonic::numpy, mod);
     }
 
 }
--- pythran/pythonic/operator_/mod.hpp
+++ pythran/pythonic/operator_/mod.hpp
@@ -8,12 +8,13 @@ namespace pythonic {
 
     namespace operator_ {
 
-        //SG: we have to deal with python modulo that always return a positive value
-        //but without using conditional because this function is also used for expression
-        //templates at the numpy level
         template <class A, class B>
-            auto mod(A const& a, B const& b) -> decltype(((a % b) + b) % b) {
-                return ((a % b) + b) % b;
+            auto mod(A const& a, B const& b)
+            -> typename std::enable_if<std::is_fundamental<A>::value and std::is_fundamental<B>::value,
+                                       decltype(a % b)>::type
+            {
+                auto t = a % b;
+                return t < 0 ? (t + b) : t;
             }
         inline double mod(double a, long b) {
             auto t = std::fmod(a, double(b));
